97779057d6a15245d83dcf7816730d166b4cde2d
Vaishnavi S. Mokashi <vaishnavimokashi1923@gmail.com>
2025-08-04 13:28:31 +0530

    Add files via upload

---
+++ Assigment_1.py
@@ -0,0 +1,13 @@
+#Task 1: Perform Basic Mathematical Operations
+a = float(input("Enter first number: "))
+b = float(input("Enter second number: "))
+print("Addition: ",a+b)
+print("Subtraction: ",a-b)
+print("Multiplication: ",a*b)
+print("Division: ",a/b)
+
+#Task 2: Create a Personalized Greeting
+text1 = input("Enter first text: ")
+text2 = input("Enter second text: ")
+full_name = text1+" "+text2
+print("Hello, " + full_name + "! Welcome to the Python Program.")
